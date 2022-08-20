9ea499e21f7264d473e907db420ed9726cff76fa
MakuS98 <111453174+MakuS98@users.noreply.github.com>
2022-08-20 12:02:18 +0300

    Create task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,69 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int data = 255;
+
+float payload = 6.153;
+
+bool flag = true;
+
+char ch = 35;
+
+int a = 10;
+
+int b = a + 100;
+
+int c = b - 20;
+
+int d = c * 3;
+
+int e = d / 2;
+
+
+
+
+
+int main()
+{
+    std::cout << "data value is : "  << data << std::endl;
+    
+    std::cout << "payload value is : "  << payload << std::endl;
+    
+    std::cout << "flag value is : "  << flag << std::endl;
+    
+    std::cout << "ch value is : "  << ch << std::endl;
+    
+    std::cout << a << std::endl;
+    
+    std::cout << b << std::endl;
+    
+    std::cout << c << std::endl;
+    
+    std::cout << d << std::endl;
+    
+    std::cout << e << std::endl;
+
+    
+    // float g = 0;
+    // float h = 1;; дві крапки з комою, но все одно працює
+    // double q = 1,5426; можно указать float і . а не ,
+    
+    
+    // int value = 3;
+    // value = value + 1; не взказан тип данних 
+    // cout << Value << endl; дві змінні з однаковою назвoю і з маленкою буквою 
+
+    
+    // int userID = 0x12; 
+    // userID += 5; не вказан тип данних, один і та ж назва змінної і якась хрінь "+="
+    // Cout << userID << Endl;
+
+    return 0;
+}
