9ca8440e65c3ad80c65f22764b397cab5f9ed9fd
MakuS98 <max.pobeg@gmail.com>
2022-09-16 13:41:26 +0300

    Add heder

---
+++ arrays_acrobatics.h
@@ -0,0 +1,34 @@
+void printAll(int a)
+ {
+  int namber[a] {};
+
+   for (int i = 1; i <= a; i++)
+   {
+    namber[i] = i;
+    std :: cout <<  namber[i] << "\n";
+
+   }
+
+ }
+ 
+void ARr(float arr[], int a)
+{
+  float b;
+  std :: cout << "Введите числа с плавующей точко " << "\n";
+   for (size_t i = 0; i < a; i++)
+   {
+    std :: cin >> b;
+    arr[i] = {b};
+
+   }
+  for (size_t i = 0; i < a; i++)
+  {
+    std :: cout << arr[i];
+  }
+  
+}
+   void arR(float collect[], int a)
+ {
+    std :: cout << collect[a] << "\n";
+ }
+ 
